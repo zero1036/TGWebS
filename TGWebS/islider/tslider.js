f0f6b71156e399f69e250214d965d432c84589e1
zero1036 <799162239@qq.com>
2015-06-19 17:32:39 +0800

    lzc:添加webWoker实例

--- TGWebS/islider/tslider.js
+++ TGWebS/islider/tslider.js
@@ -1,7 +1,39 @@
 ﻿; (function () {
     var ts = function () {
         var ts = function (opts) {
+            this.wrap = document.getElementById('block');
+            this._renderHTML();
             this._bindHandler();
+
+        };
+        ts.prototype._renderHTML = function () {
+            this.outer && (this.outer.innerHTML = '');
+            // initail ul element
+            var outer = this.outer || document.createElement('ul');
+            outer.style.cssText = 'height:' + this.height + 'px;width:' + this.width + 'px;margin:0;padding:0;list-style:none;';
+            // storage li elements, only store 3 elements to reduce memory usage
+            this.els = [];
+            for (var i = 0; i < 3; i++) {
+                var li = document.createElement('li');
+                li.className = this.type === 'dom' ? 'islider-dom' : 'islider-pic';
+                li.style.cssText = 'height:' + this.height + 'px;width:' + this.width + 'px;';
+                //this.els.push(li);
+                //// prepare style animation
+                //this._animateFunc(li, this.axis, this.scale, i, 0);
+                //if (this.isVertical && (this._opts.animateType === 'rotate' || this._opts.animateType === 'flip')) {
+                //    this._renderItem(li, 1 - i + this.slideIndex);
+                //} else {
+                //    this._renderItem(li, i - 1 + this.slideIndex);
+                //}
+                li.innerHTML = "Tgor";
+                outer.appendChild(li);
+            }
+            //this._initLoadImg();
+            // append ul to div#canvas
+            if (!this.outer) {
+                this.outer = outer;
+                this.wrap.appendChild(outer);
+            }
         };
         ts.prototype._bindHandler = function () {
             var outer = this.outer;
@@ -27,7 +59,7 @@
                     this.startHandler(evt);
                     break;
                 case device.moveEvt:
-                    //this.moveHandler(evt);
+                    this.moveHandler(evt);
                     break;
                 case device.endEvt:
                     //this.endHandler(evt);
@@ -50,18 +82,47 @@
             //if (this.fixPage) {
             //    evt.preventDefault();
             //}
-            //var device = this._device();
-            //this.isMoving = true;
+            var device = this._device();
+            this.isMoving = true;
             //this.pause();
-            //this.onslidestart && this.onslidestart();
-            //this.log('Event: beforeslide');
-            //this.startTime = new Date().getTime();
-            //this.startX = device.hasTouch ? evt.targetTouches[0].pageX : evt.pageX;
-            //this.startY = device.hasTouch ? evt.targetTouches[0].pageY : evt.pageY;
-            //this._startHandler && this._startHandler(evt);
+            this.onslidestart && this.onslidestart();
+            this.log('Event: beforeslide');
+            this.startTime = new Date().getTime();
+            this.startX = device.hasTouch ? evt.targetTouches[0].pageX : evt.pageX;
+            this.startY = device.hasTouch ? evt.targetTouches[0].pageY : evt.pageY;
+            this._startHandler && this._startHandler(evt);
             console.log("start");
         };
 
+        iSlider.prototype.moveHandler = function (evt) {
+            if (this.isMoving) {
+                var device = this._device();
+                var len = this.data.length;
+                var axis = this.axis;
+                var reverseAxis = this.reverseAxis;
+                var offset = {
+                    X: device.hasTouch ? evt.targetTouches[0].pageX - this.startX : evt.pageX - this.startX,
+                    Y: device.hasTouch ? evt.targetTouches[0].pageY - this.startY : evt.pageY - this.startY
+                };
+                var res = this._moveHandler ? this._moveHandler(evt) : false;
+                if (!res && Math.abs(offset[axis]) - Math.abs(offset[reverseAxis]) > 10) {
+                    evt.preventDefault();
+                    this.onslide && this.onslide(offset[axis]);
+                    this.log('Event: onslide');
+                    if (!this.isLooping) {
+                        if (offset[axis] > 0 && this.slideIndex === 0 || offset[axis] < 0 && this.slideIndex === len - 1) {
+                            offset[axis] = this._damping(offset[axis]);
+                        }
+                    }
+                    for (var i = 0; i < 3; i++) {
+                        var item = this.els[i];
+                        item.style.webkitTransition = 'all 0s';
+                        this._animateFunc(item, axis, this.scale, i, offset[axis]);
+                    }
+                }
+                this.offset = offset;
+            }
+        };
 
         ts.prototype._device = function () {
             var hasTouch = !!('ontouchstart' in window || window.DocumentTouch && document instanceof window.DocumentTouch);
---
+++ TGWebS/javascript/fibonacci.js
@@ -0,0 +1,7 @@
+﻿var fibonacci = function (n) {
+    return n < 2 ? n : arguments.callee(n - 1) + arguments.callee(n - 2);
+};
+onmessage = function (event) {
+    var n = parseInt(event.data, 10);
+    postMessage(fibonacci(n));
+};
