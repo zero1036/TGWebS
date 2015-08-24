45a693f7315dbfc2e213b375763a6de924ac64a5
zero1036 <799162239@qq.com>
2015-08-24 10:47:11 +0800

    lzc:加入加载器实例

---
+++ TGWebS/nodeJS/webpack/loaders/entry.js
@@ -0,0 +1,1 @@
+﻿require("!style!css!less!./style.less");
---
+++ TGWebS/nodeJS/webpack/loaders/webpack.config.js
@@ -0,0 +1,15 @@
+﻿module.exports = {
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loader: "style!css!less"
+            }
+        ]
+    }
+};
