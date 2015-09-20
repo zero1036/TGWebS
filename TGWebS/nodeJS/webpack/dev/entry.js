eed554cebed5161818ac24af6de293424a7f0ed3
zero1036 <t799162239@hotmail.com>
2015-09-21 07:31:14 +0800

    lzc:添加webpack开发模式

---
+++ TGWebS/nodeJS/webpack/dev/entry.js
@@ -0,0 +1,8 @@
+﻿//require("!style!css!./style.css");
+var content = require("./content.js");
+
+function a() {
+    document.write(content);
+    console.log("abc1dlasflsa23");
+};
+a();
---
+++ TGWebS/nodeJS/webpack/dev/webpack.config.js
@@ -0,0 +1,32 @@
+﻿//var path = require("path");
+//module.exports = {
+//    entry: {
+//        //main: ['webpack/hot/dev-server', './entry']
+//        app: ["./entry.js"]
+//    },
+//    output: {
+//        path: path.resolve(__dirname, "build"),
+//        publicPath: "/assets/",
+//        filename: "bundle.js"
+//    }
+//};
+var webpack = require('webpack');
+
+var path = require("path");
+module.exports = {
+    entry: {
+        //main: ['webpack/hot/dev-server', './entry']
+        app: ['webpack-dev-server/client?http://192.168.0.20:3000',
+        'webpack/hot/dev-server',
+        './entry.js']
+    },
+    output: {
+        path: path.resolve(__dirname, "build"),
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
