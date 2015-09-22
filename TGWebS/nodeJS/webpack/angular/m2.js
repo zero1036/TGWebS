60882deb5cda9405f61ac100d6023002d828db9d
zero1036 <t799162239@hotmail.com>
2015-09-23 07:27:49 +0800

    lzc:添加webpack angular例子

---
+++ TGWebS/nodeJS/webpack/angular/m2.js
@@ -0,0 +1,24 @@
+﻿var MySalute = require("./m1");
+require("../../../js/angular/angular");
+require("../../../js/angular/angular-cookies");
+require("../../../js/angular/angular-route");
+
+var app = angular.module("app", ['ngCookies', 'ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+    .when('/user', {
+        template: '<p>naocao</p>',
+        //controller: 'userController'
+    })
+    .when('/company', {
+        templateUrl: 'company.html',
+        //controller: 'companyController'
+    });
+}]);
+
+app.controller("ctrl", function ($scope) {
+    $scope.name = "tg";
+});
+
+document.write(MySalute + "world!");
